cc965567ecde44b344898ea1cee4610754398743
Erick N <batistasd678@gmail.com>
2022-09-11 15:51:09 -0300

    Direcionado para PROD

--- src/main/java/com/snoweegamecorp/backend/BackendApplication.java
+++ src/main/java/com/snoweegamecorp/backend/BackendApplication.java
@@ -12,7 +12,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.snoweegamecorp.backend.model.UserModel;
 import com.snoweegamecorp.backend.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 @EntityScan(basePackages = {
 			"com.snoweegamecorp.backend.model"
@@ -22,6 +26,11 @@ import com.snoweegamecorp.backend.repository.UserRepository;
 			})
 public class BackendApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
