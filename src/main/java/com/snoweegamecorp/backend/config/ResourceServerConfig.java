9966d7318e87a4e729b359a9db6825c569ee97ae
Erick N <batistasd678@gmail.com>
2022-10-08 20:59:45 -0300

    Liberado o H2 da verificacao de autenticacao

--- src/main/java/com/snoweegamecorp/backend/config/ResourceServerConfig.java
+++ src/main/java/com/snoweegamecorp/backend/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.snoweegamecorp.backend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
@@ -17,7 +20,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
     private static final String[] OPERATOR_OR_ADMIN = {"/users/**"};
     private static final String[] ADMIN = {"/users/**","/permissions/**"};
 
@@ -28,6 +34,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //Liberar H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("dev")){
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
