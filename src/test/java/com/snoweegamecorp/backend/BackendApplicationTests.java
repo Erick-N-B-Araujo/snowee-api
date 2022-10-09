47e9b5fb5bb49dbb150f7b6c4b54181a424c741e
Erick N <batistasd678@gmail.com>
2022-10-08 22:25:11 -0300

    Adicionado Token Util para obter o token do usuario, e teste automatizado para gerar token

--- src/test/java/com/snoweegamecorp/backend/BackendApplicationTests.java
+++ src/test/java/com/snoweegamecorp/backend/BackendApplicationTests.java
@@ -1,13 +1,36 @@
 package com.snoweegamecorp.backend;
 
+import com.snoweegamecorp.backend.utils.TokenUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BackendApplicationTests {
+
+	private String adminUsername;
+	private String adminPassword;
+	@Autowired
+	private TokenUtil tokenUtil;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp() throws Exception{
+		adminUsername = "batistasd678@gmail.com";
+		adminPassword = "1234";
+	}
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void getToken() throws Exception {
+		String accessToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
 	}
 
 }
---
+++ src/test/java/com/snoweegamecorp/backend/utils/TokenUtil.java
@@ -0,0 +1,43 @@
+package com.snoweegamecorp.backend.utils;
+
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Component
+public class TokenUtil {
+    private String clientId = "snoweeapi";
+    private String clientSecret  = "snoweeapi";
+    public String obtainAccessToken(MockMvc mvc, String username, String password) throws Exception {
+
+        MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "password");
+        params.add("client-id", clientId);
+        params.add("username", username);
+        params.add("password", password);
+
+        ResultActions result = mvc
+                .perform(post("/oauth/token")
+                        .params(params)
+                        .with(httpBasic(clientId,clientSecret))
+                        .accept("application/json;charset=UTF-8"))
+                        .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+
+        String resultString = result
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resultString).get("access_token").toString();
+    }
+}
