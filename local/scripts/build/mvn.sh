f3becbd9a9884153c337dc70a2cbd1725f201ba6
root <root@vps.server.local>
2023-04-05 13:51:22 -0300

    Att Jenkinsfile

--- local/scripts/build/mvn.sh
+++ local/scripts/build/mvn.sh
@@ -4,4 +4,4 @@ echo "Maven command..."
 
 WORKSPACE=/var/lib/jenkins/workspace/Backend
 
-docker run --rm -v $WORKSPACE/java-app:/app  -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/local/java-app:/app  -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
